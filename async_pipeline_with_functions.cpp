cbc2f1407482ae495eeee8b544bd1c4377d2ef1a
Dmitrii Khizbullin <dmitrii.khizbullin@gmail.com>
2020-09-17 03:58:29 +0300

    More comments

--- async_pipeline_with_functions.cpp
+++ async_pipeline_with_functions.cpp
@@ -89,31 +89,65 @@ void visualize(std::future<std::pair<size_t, std::string>>&& future_input,
 }
 
 
+// The entry point to the app.
 int main()
 {
+    // Capture the moment we start the app.
     const auto start_time = std::chrono::high_resolution_clock::now();
 
-    size_t pipeline_depth = 2; // Number of processors except visualizer
+    // Here we explicitly specify the number of processors except visualizer.
+    size_t pipeline_depth = 2;
 
+    // We need to keep track of visualizer's future objects which essentially
+    // return void, however a future object also keeps track of the event of
+    // sample processing end. This will allow us to prevent the overflow of
+    // the pipeline further below.
     std::deque<std::future<void>> visualize_futures;
 
+    // An aid to guarantee serial visualization of incoming samples is
+    // an atomic index variable.
     std::atomic<unsigned long> current_idx { 0 };
 
+    // Now we are ready to fill our pipeline with samples. Let's limit the number
+    // of samples to 100 to showcase how we can gracefully stop the pipeline
+    // after all samples are visualized.
     for (size_t idx = 0; idx < 100; idx++)
     {
+        // Here we create an input to the pipeline. Let's make it a string.
         auto input_str = std::string("input_string_") + std::to_string(idx);
+        // We'd like to wrap the input into a future object which is available
+        // immediately. This trick allows us to have a uniform signature of
+        // functions func#, that takes a future object and returns a value
+        // object, which in its turn will be wrapped into a brand new future object
+        // on return from std::async.
         std::promise<std::pair<size_t, std::string> > promise_0;
         auto future_0 = promise_0.get_future();
+        // Notice that we pass not only the payload to our processors, but an index
+        // of a sample. This allows us to figure out the order to visualize samples.
+        // Otherwise sample N+1 may be ready earlier than sample N.
         promise_0.set_value(std::make_pair(idx, input_str));
 
+        // Here we go launching our first stage of the pipeline. Notice that we
+        // run std::async in std::launch::async mode to immediately start compute.
+        // We also have to move the future object since it is not copyable.
         auto future_1 = std::async(std::launch::async, &func1, std::move(future_0));
 
+        // Since the future object from the first stage is available immediately,
+        // we launch the second stage right away.
         auto future_2 = std::async(std::launch::async, &func2, std::move(future_1));
 
+        // The visualization stage is also enqueued without delay. This is where
+        // asynchronous operations shine. The atomic current_idx is passed here by
+        // reference to ensure sequential visualisation of samples despite multiple
+        // visualization threads will be running concurrently.
         auto future_vis = std::async(std::launch::async, &visualize, std::move(future_2),
             std::ref(start_time), std::ref(current_idx));
 
+        // We append the dummy futures into a double-ended queue.
         visualize_futures.push_back(std::move(future_vis));
+        // And check that the pipeline is full. We do not want our pipeline to launch
+        // dozens of threads ahead of time since we know that the visualization will
+        // anyway run in 1 second intervals.
         if (visualize_futures.size() > pipeline_depth)
         {
             // At this point the main thread will call a destructor of the oldest future,
@@ -125,11 +159,13 @@ int main()
     }
 
     std::cout << "Waiting to finish..." << std::endl;
+    // Here we gracefully stop the pipeline by waiting for all threads to join.
     for (auto& fut : visualize_futures)
     {
         fut.get();
     }
 
+    // And we are done!
     std::cout << "Finished!" << std::endl;
     return 0;
 }
